0c819f782759c1473858da4dd5e87ed098a2165a
yukz-yumi <33631494+yukz-yumi@users.noreply.github.com>
2021-08-19 17:31:09 +0800

    审核任务认领记录时产生审核记录

---
+++ daodaoping/src/main/java/com/yukz/daodaoping/common/task/EndTaskJob.java
@@ -0,0 +1,49 @@
+package com.yukz.daodaoping.common.task;
+
+import com.yukz.daodaoping.app.task.enums.TaskAcceptionStatusEunm;
+import com.yukz.daodaoping.app.task.enums.TaskVerifyStatusEnum;
+import com.yukz.daodaoping.common.task.service.CheckTaskAcceptJobService;
+import com.yukz.daodaoping.task.domain.TaskAcceptInfoDO;
+import com.yukz.daodaoping.task.service.TaskAcceptInfoService;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class EndTaskJob implements Job{
+    private static final Logger logger = LoggerFactory.getLogger(EndTaskJob.class);
+	@Autowired
+	private TaskAcceptInfoService taskAcceptInfoService;
+	@Autowired
+    private CheckTaskAcceptJobService checkTaskAcceptJobService;
+
+    @Override
+    public void execute(JobExecutionContext arg0) throws JobExecutionException {
+    	logger.info("开始审核任务认领记录");
+        try {
+            Map<String, Object> query = new HashMap<String, Object>();
+            query.put("taskStatus", TaskAcceptionStatusEunm.END.getStatus());
+            query.put("verifyStatus", TaskVerifyStatusEnum.UNVERIFIED.getStatus());
+            List<TaskAcceptInfoDO> taskAcceptInfoList = taskAcceptInfoService.list(query);
+            if (null != taskAcceptInfoList && taskAcceptInfoList.size()>0) {
+                for (TaskAcceptInfoDO taskAccept : taskAcceptInfoList) {
+                    checkTaskAcceptJobService.checkTaskAccept(taskAccept);
+                }
+                logger.info("成功审核任务认领记录");
+            }
+        } catch (Exception e) {
+            logger.error("定时审核任务认领出错"+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- daodaoping/src/main/java/com/yukz/daodaoping/common/task/service/impl/CheckTaskAcceptJobServiceImpl.java
+++ daodaoping/src/main/java/com/yukz/daodaoping/common/task/service/impl/CheckTaskAcceptJobServiceImpl.java
@@ -1,11 +1,14 @@
 package com.yukz.daodaoping.common.task.service.impl;
 
 import com.yukz.daodaoping.app.task.enums.TaskVerifyStatusEnum;
+import com.yukz.daodaoping.approve.domain.VerifyRecordInfoDO;
+import com.yukz.daodaoping.approve.service.VerifyRecordInfoService;
 import com.yukz.daodaoping.common.task.service.CheckTaskAcceptJobService;
 import com.yukz.daodaoping.task.domain.TaskAcceptInfoDO;
 import com.yukz.daodaoping.task.domain.TaskApplyInfoDO;
 import com.yukz.daodaoping.task.service.TaskAcceptInfoService;
 import com.yukz.daodaoping.task.service.TaskApplyInfoService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,10 +21,13 @@ public class CheckTaskAcceptJobServiceImpl implements CheckTaskAcceptJobService 
     private TaskApplyInfoService taskApplyInfoService;
     @Autowired
     private TaskAcceptInfoService taskAcceptInfoService;
+    @Autowired
+    private VerifyRecordInfoService verifyRecordInfoService;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void checkTaskAccept(TaskAcceptInfoDO taskAccept) throws Exception{
+        Date now = new Date();
         //校验接单人提交的凭证是否有效
         boolean verify = verifyCertification(taskAccept);
         // 通过则更新审核状态为已审核
@@ -33,7 +39,23 @@ public class CheckTaskAcceptJobServiceImpl implements CheckTaskAcceptJobService 
             upTaskAccept.setVerifyStatus(TaskVerifyStatusEnum.REJECTED.getStatus());
         }
         upTaskAccept.setWorkerId(0l);
+        upTaskAccept.setGmtModify(now);
         taskAcceptInfoService.update(upTaskAccept);
+
+        //产生审核记录
+        VerifyRecordInfoDO verifyRecord = new VerifyRecordInfoDO();
+        verifyRecord.setAgentId(taskAccept.getAgentId());
+        verifyRecord.setExpireTime(now);
+        verifyRecord.setGmtCreate(now);
+        verifyRecord.setGmtModify(now);
+        verifyRecord.setTaskAcceptId(taskAccept.getId());
+        verifyRecord.setTaskId(taskAccept.getTaskId());
+        verifyRecord.setWorkerId(0l);
+        verifyRecord.setOperation(upTaskAccept.getVerifyStatus());
+        if (!verify) {
+            verifyRecord.setInvalidateReason("系统自动审核未通过");
+        }
+        verifyRecordInfoService.save(verifyRecord);
 
         //查询任务申请记录，锁定任务申请，防止更新数量时并发
         TaskApplyInfoDO taskApplyInfo = taskApplyInfoService.getByIdForupdate(taskAccept.getTaskId());
@@ -44,11 +66,16 @@ public class CheckTaskAcceptJobServiceImpl implements CheckTaskAcceptJobService 
         TaskApplyInfoDO upTaskApply = new TaskApplyInfoDO();
         upTaskApply.setId(taskApplyInfo.getId());
         upTaskApply.setCompletedNumber(completeNum);
-        upTaskApply.setGmtModify(new Date());
+        upTaskApply.setGmtModify(now);
         taskApplyInfoService.update(upTaskApply);
     }
 
     private boolean verifyCertification(TaskAcceptInfoDO taskAccept) {
+        Date now = new Date();
+        boolean rtn = false;
+        if (StringUtils.isNotBlank(taskAccept.getCertificationUrl())) {
+            rtn = true;
+        }
         return true;
     }
 }
